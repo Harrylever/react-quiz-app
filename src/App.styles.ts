bee1f568aa5e315055b0b38b705f3c4f8bebc43a
Harrylever <ukanah15thdean@gmail.com>
2022-11-11 14:15:40 +0100

    Initial Commit

---
+++ src/App.styles.ts
@@ -0,0 +1,291 @@
+import styled, { createGlobalStyle } from "styled-components";
+// import BGImage from "./images/nattu-adnan-unsplash.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+	html {
+		height: 100%;
+
+    * {
+      margin: 0;
+      padding: 0;
+    }
+	}
+
+	body {
+    background-color: #e6f3ff;
+    margin: 0;
+		padding: 0;
+		display: flex;
+		justify-content: center;
+    overflow-x: hidden; 
+	}
+
+	* {
+		box-sizing: border-box;
+		font-family: 'Catamaran', sans-serif;
+	}
+`;
+
+export const NavWrapper = styled.nav`
+  background-color: #fff;
+  width: 100vw;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 40px;
+  
+  > .nav_cont_1 {
+    max-width: 1200px;
+    width: 60%;
+    padding: 20px 0;
+
+    h1 {
+      font-family: Rubik, sans-serif;
+      background-image: linear-gradient(180deg, #000, #000);
+      background-size: 100%;
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      -moz-background-clip: text;
+      -moz-text-fill-color: transparent;
+      font-size: 3rem;
+      font-weight: 500;
+      text-align: left;
+    }
+
+    @media (max-width: 768px) {
+      h1 {
+        text-align: left;
+        font-size: 2.6rem;
+      }
+    }
+
+    @media (max-width: 500px) {
+      h1 {
+        text-align: left;
+        font-size: 1.6rem;
+      }
+    }
+  }
+
+  > .nav_cont_2 {
+    display: flex;
+    flex-direction: column;
+    font-size: 13px;
+
+    p {
+      display: flex;
+      align-items: center;
+      font-size: .8rem;
+
+      img {
+        width: 20%;
+        margin-left: 5px;
+      }
+    }
+
+    a {
+      text-decoration: underline;
+      color: #000;
+      font-weight: 600;
+      font-size: .7rem;
+    }
+
+    @media (max-width: 768px) {
+      p {
+        font-size: .6rem;
+      }
+
+      a {
+        font-size: .56rem;
+      }
+    }
+  }
+`;
+
+export const Wrapper = styled.div`
+  padding: 30px 0;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    align-items: center;
+    gap: 50px;
+    flex-direction: column;
+  }
+
+  > * {
+    /* border: 1px solid black; */
+  }
+
+  > p {
+    color: #000;
+  }
+
+  .score {
+    color: #000;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  .start,
+  .next {
+    cursor: pointer;
+    background: linear-gradient(170deg, #595959, #1a1a1a, #000, #000);
+    border: none;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+    color: #fff;
+  }
+
+  .start {
+    max-width: 200px;
+  }
+`;
+
+export const GameSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  /* border: 1px solid #000; */
+  width: 65%;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const DifficultySelect = styled.select`
+  margin: 5px 3px 0px;
+  border: 2px solid #ccc;
+  background-color: #efefef;
+  border-radius: 4px;
+  width: 130px;
+  font-size: 14px;
+  outline: none;
+  padding: 2px 0;
+  text-align: center;
+`;
+
+export const SetDifficultyView = styled.div`
+  margin-top: 5px;
+  border: 2px solid #ccc;
+  background-color: #efefef;
+  border-radius: 4px;
+  width: 130px;
+  font-size: 14px;
+  outline: none;
+  text-align: center;
+  padding: 2px 0;
+`;
+
+export const ExtraContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 35%;
+
+  .art_sec_01 {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 20px;
+
+    > h1 {
+      width: 55%;
+      font-size: 1.3rem;
+      color: #2f3a43;
+    }
+    > div {
+      position: relative;
+      max-width: 125px;
+      max-height: 125px;
+      min-height: 125px;
+      width: 40%;
+
+      > p {
+        position: absolute;
+        bottom: -14%;
+        opacity: .7;
+        left: 9%;
+        font-style: italic;
+        font-size: 12px;
+        color: #AB6800;
+        font-weight: 700;
+      }
+    }
+    .profile_img_cont {
+      border-radius: 50%;
+      max-width: 125px;
+      max-height: 125px;
+      min-height: 125px;
+      width: 100%;
+      overflow: hidden;
+
+      img {
+        width: 105%;
+        object-fit: cover;
+        object-position: -50%;
+      }
+    }
+  }
+  .created_with_cont {
+    padding-top: 10px;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+
+    a {
+      text-decoration: none;
+      color: #000;
+      font-size: 17px;
+      width: 70%;
+      opacity: 0.6;
+      border: 2px solid black;
+
+      &:hover {
+        opacity: .84;
+      }
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      margin-bottom: 7px;
+
+      > div {
+        max-width: 60px;
+        min-width: 60px;
+        width: 100%;
+        max-height: 60px;
+        min-height: 60px;
+        height: 100%;
+
+        img {
+          width: 100%;
+        }
+      }
+
+      @media (max-width: 768px) {
+        margin: 0 auto;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    max-width: 500px;
+    width: 100%;
+    padding: 10px;
+    text-align: center;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,219 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import {
+  DifficultySelect,
+  ExtraContent,
+  GameSection,
+  GlobalStyle,
+  NavWrapper,
+  SetDifficultyView,
+  Wrapper,
+} from "./App.styles";
+
+import profileImg from './images/Harrylever.jpg'
+import reactLogo from './images/react_log.png';
+import triviaLogo from './images/trivia_logo.png';
+import github_logo from './images/github_logo.png';
+
+// Import FetchQuiz Question
+import { fetchQuizQuestions } from "./API";
+
+// Import question diff types
+import { QuestionState, Difficulty } from "./API";
+
+// Import QuestionCard Component
+import QuestionCard from "./components/QuestionCard";
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  // App States
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const [difficultyLevel, setDifficultyLevel] = useState('easy');
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    try {
+      const newQuestions = await fetchQuizQuestions(
+        TOTAL_QUESTIONS,
+        difficultyLevel
+      );
+      if (newQuestions) setQuestions(newQuestions);
+      setScore(0);
+      setUserAnswers([]);
+      setNumber(0);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching Question: ", error);
+    }
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // Users Answer
+      const answer = e.currentTarget.value;
+
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+
+      // Add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    // Move unto the next question if not the last
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <NavWrapper>
+        <div className="nav_cont_1">
+          <h1>REACT QUIZ</h1>
+        </div>
+        <div className="nav_cont_2">
+          <p>Check out the code  <img src={github_logo} alt="" /></p>
+          <a href="https://">GitHub: React Quiz App</a>
+        </div>
+      </NavWrapper>
+      <Wrapper>
+        <GameSection>
+          <p>Test your knowledge of everything.</p>
+
+          <div>
+            {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+              <>
+                <p
+                  style={{
+                    color: "#242222cd",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                    marginTop: "3px",
+                  }}
+                >
+                  Choose a Difficulty Level
+                </p>
+                <DifficultySelect
+                  name="difficulty"
+                  onChange={(e) => {
+                    setDifficultyLevel(e.target.value);
+                  }}
+                >
+                  <option value={Difficulty.EASY}>{Difficulty.EASY}</option>
+                  <option value={Difficulty.MEDIUM}>{Difficulty.MEDIUM}</option>
+                  <option value={Difficulty.HARD}>{Difficulty.HARD}</option>
+                </DifficultySelect>
+              </>
+            ) : null}
+            {!loading && !gameOver && (
+              <SetDifficultyView>{difficultyLevel}</SetDifficultyView>
+            )}
+          </div>
+          {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+            <button className="start" onClick={startTrivia}>
+              Start
+            </button>
+          ) : null}
+          {!gameOver ? <p className="score">Score: {score}</p> : null}
+          {loading && <p>Loading Questions ...</p>}
+
+          <div>
+            {!loading && !gameOver && (
+              <QuestionCard
+                questionNr={number + 1}
+                totalQuestions={TOTAL_QUESTIONS}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswers ? userAnswers[number] : undefined}
+                callback={checkAnswer}
+              />
+            )}
+          </div>
+          {!gameOver &&
+          !loading &&
+          userAnswers.length === number + 1 &&
+          number !== TOTAL_QUESTIONS - 1 ? (
+            <button className="next" onClick={nextQuestion}>
+              Next Question
+            </button>
+          ) : null}
+        </GameSection>
+        <ExtraContent>
+          <article>
+            <div className="art_sec_01">
+              <h1>
+                Author: Ukanah Dean <em>(Harrylever)</em>
+              </h1>
+              <div>
+                <div className="profile_img_cont">
+                  <a href="https://github.com/Harrylever">
+                    <img src={profileImg} alt="" />
+                  </a>
+                </div>
+                <p>View Github Profile</p>
+              </div>
+            </div>
+            <div className="art_sec_02">
+              <p>
+                A simple quiz generator to assess your knowledge of general
+                world topics.
+              </p>
+              <div className="created_with_cont">
+                <h3>Created with</h3>
+                <a href="https://reactjs.org/">
+                  <div>
+                    <p>React-JS</p>
+                    <div>
+                      <img src={reactLogo} alt="" />
+                    </div>
+                  </div>
+                </a>
+                <a href="https://opentdb.com/api_config.php">
+                  <div>
+                    <p>Open Trivia Database API</p>
+                    <div>
+                      <img src={triviaLogo} alt="" />
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </article>
+        </ExtraContent>
+      </Wrapper>
+    </>
   );
-}
+};
 
 export default App;
